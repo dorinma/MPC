abe8e29bb4bde4877b029bdc36284d4ea78a2d19
bshahar <48634205+bshahar@users.noreply.github.com>
2021-12-23 21:13:52 +0200

    sharedsecret

---
+++ MPCDataClient/DataService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MPCDataClient
+{
+	
+	public class DataService
+	{
+		public List<UInt16> ServerAList;
+		public List<UInt16> ServerBList;
+		public int operation;
+		public int randomRange;
+
+		public DataService()
+		{
+			this.ServerAList = new List<UInt16>();
+			this.ServerBList = new List<UInt16>();
+			this.randomRange = 65535;
+		}
+
+
+		public void generateSecretShares(List<UInt16> input)
+		{
+			Random rnd = new Random();
+
+			foreach (UInt16 i in input)
+			{
+				UInt16 shareA = (UInt16)rnd.Next(0, this.randomRange+1); // creates a number between 1 and 12
+				this.ServerAList.Add(shareA);
+				UInt16 shareB = (UInt16)(i + this.randomRange - shareA);
+				this.ServerBList.Add(shareB);
+			}
+		}
+	}
+}
---
+++ MPCDataClient/UserService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MPCDataClient
+{
+    class UserService
+    {
+        static void Main(string[] args)
+        {
+            List<UInt16> data = readData();
+            int operation = readOperation();
+            DataService dataService = new DataService();
+            dataService.generateSecretShares(data);
+        }
+
+        public static List<UInt16> readData()
+        {
+            Console.WriteLine("Insert data file path");
+            string path = "";
+            path = Console.ReadLine();
+            return readFromFile(path);
+
+        }
+
+        public static int readOperation()
+        {
+            Console.WriteLine("Insert the number of operation you want to perform:\n1. merge\n2. find the K'th element\n3. sort");
+            int operation = 0;
+            operation = Convert.ToInt32(Console.ReadLine());
+            return operation;
+
+        }
+
+        public static List<UInt16> readFromFile(string path)
+        {
+            List<UInt16> output = new List<UInt16>();
+            try
+            {
+                using (var reader = new StreamReader(path))
+                {
+                    UInt16 curr;
+                    reader.ReadLine(); //skip column name
+                    while (!reader.EndOfStream)
+                    {
+                        if (!UInt16.TryParse(reader.ReadLine(), out curr))
+                        {
+                            return null;
+                        }
+                        output.Add(curr);
+                    }
+                }
+                return output;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Reading failed - {0}", e.Message);
+                return null;
+            }
+        }
+
+    }
+}
