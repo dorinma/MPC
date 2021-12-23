fdfaeb36a986b12e6c724f479d4934d7a32817c9
hodaya1 <48647966+hodaya1@users.noreply.github.com>
2021-12-23 17:24:34 +0200

    server recive messege :)

--- MPCDataClient/DataService.cs
+++ MPCDataClient/DataService.cs
@@ -10,9 +10,13 @@ namespace MPCDataClient
     {
         static void Main(string[] args)
         {
-            string IP = "127.0.0.1"; //eden 100.64.182.7
+            string IP = "172.16.205.108"; //eden 100.64.182.7
             Connect(IP);
             Communication.SendRequest("Hey bitchessss");
+            while(true) {
+                if (Console.Read() == 'q')
+                    break;
+            }
         }
         
         private static void Connect(string IP)
