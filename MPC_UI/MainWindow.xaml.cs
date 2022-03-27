b0d84f7be27b22780603f5e5d35f8881caabcdf8
dorinma <dorinma@post.bgu.ac.il>
2022-03-27 16:40:46 +0300

    Add inputs & input validation

--- MPC_UI/MainWindow.xaml.cs
+++ MPC_UI/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
 using System.Text.RegularExpressions;
 using Microsoft.Win32;
+using System.Net;
+using System.IO;
+
+using MPC_UI.ViewModel;
 
 namespace MPC_UI
 {
@@ -23,9 +14,14 @@ namespace MPC_UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainDataContext mainDataContext;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            mainDataContext = new MainDataContext();
+            DataContext = mainDataContext;
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
@@ -34,17 +30,47 @@ namespace MPC_UI
             openFileDialog.Filter = "Csv files (*.csv)|*.csv|All files (*.*)|*.*";
             if (openFileDialog.ShowDialog() == true)
                 inFile.Text = openFileDialog.FileName;
+            if (Path.GetExtension(openFileDialog.FileName) != ".csv")
+            {
+                MessageBox.Show("Please choose .csv file.");
+            }
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
         {
-           
+            if(ValidateInput())
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Please make sure all information is valid.");
+            }
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private bool ValidateInput()
+        {
+            return ValidateIP(mainDataContext.IP1) && ValidateIP(mainDataContext.IP2)
+                && ValidatePort(mainDataContext.Port1) && ValidatePort(mainDataContext.Port2)
+                && mainDataContext.ParticipantsNum > 0 && mainDataContext.ParticipantsNum < 100 //TODO how many??
+                && inFile.Text != "";
+        }
+
+        private bool ValidateIP(string ip)
+        {
+            IPAddress ipAddress;
+            return IPAddress.TryParse(ip, out ipAddress);
+        }
+
+        private bool ValidatePort(int port)
+        {
+            return port > -1 && port < 65536;
         }
     }
 }
---
+++ MPC_UI/ViewModel/MainDataContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MPC_UI.ViewModel
+{
+    class MainDataContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string ip1, ip2;
+        int port1, port2, participants;
+
+        public MainDataContext()
+        {
+            ip1 = "";
+            ip2 = "";
+            port1 = 0;
+            port2 = 0;
+            participants = 0;
+        }
+
+        public string IP1
+        {
+            get
+            {
+                return ip1;
+            }
+            set
+            {
+                ip1 = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("IP1"));
+            }
+        }
+
+        public string IP2
+        {
+            get
+            {
+                return ip2;
+            }
+            set
+            {
+                ip2 = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("IP2"));
+            }
+        }
+        public int Port1
+        {
+            get
+            {
+                return port1;
+            }
+            set
+            {
+                port1 = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Port1"));
+            }
+        }
+        public int Port2
+        {
+            get
+            {
+                return port2;
+            }
+            set
+            {
+                port2 = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Port2"));
+            }
+        }
+        public int ParticipantsNum
+        {
+            get
+            {
+                return participants;
+            }
+            set
+            {
+                participants = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("ParticipantsNum"));
+            }
+        }
+
+    }
+}
