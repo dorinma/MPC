b9d5aeb26cbf5ff32c79be2e74f563a5fecfdbd9
hodaya1 <48647966+hodaya1@users.noreply.github.com>
2022-05-13 14:09:20 +0300

    merge comm func in compute

--- MPCServer/Computer.cs
+++ MPCServer/Computer.cs
@@ -44,18 +44,52 @@ namespace MPCServer
             //actually logic
 
             // first level - dcf between each pair values
-            // second level - sum results
-
             int numOfElement = data.Length;
-            uint[] sharesIndexes = new uint[numOfElement];
-
             int n = sortRandomRequest.n;
-            string[] dcfKeys = sortRandomRequest.dcfKeys;
-            string[] dcfAesKeys = sortRandomRequest.dcfAesKeys;
+            // todo check n > numofelement
+
             uint[] sumValuesMasks = SumServersShares(numOfElement);
 
-            uint[] diffValues = SumEachPairValues(sumValuesMasks, numOfElement);
+            uint[] diffValues = DiffEachPairValues(sumValuesMasks, numOfElement);
 
+            uint[] sharesIndexes = ComputeIndexesShares(diffValues, numOfElement, n);
+
+            //DEBUG
+            Console.WriteLine("shares Indexes");
+            for (int i = 0; i < sharesIndexes.Length; i++)
+            {
+                Console.WriteLine("\t" + sharesIndexes[i] + "\t");
+            }
+
+            // second level - sum results
+            uint[] sumIndexesMasks = sumIndexesWithMasks(sharesIndexes);
+
+            // third level - compare eatch value result to all possible indexes and placing in the returned list
+            uint[] sortList = ComputeResultsShares(sumIndexesMasks, sumValuesMasks, n);
+
+            return sortList;
+        }
+
+        private uint[] ComputeResultsShares(uint[] sumIndexesMasks, uint[] sumValuesMasks, int numOfElement)
+        {
+            uint[] sortList = new uint[numOfElement];
+            string[] dpfKeys = sortRandomRequest.dpfKeys;
+            string[] dpfAesKeys = sortRandomRequest.dpfAesKeys;
+            for (int i = 0; i < numOfElement; i++)
+            {
+                for (int j = 0; j < numOfElement; j++)
+                {
+                    sortList[j] += dpfAdapter.Eval(instance, dpfKeys[i], dpfAesKeys[i], sumIndexesMasks[i] - (uint)j, sumValuesMasks[i]);
+                }
+            }
+            return sortList;
+        }
+
+        private uint[] ComputeIndexesShares(uint[] diffValues, int numOfElement, int n)
+        {
+            uint[] sharesIndexes = new uint[numOfElement];
+            string[] dcfKeys = sortRandomRequest.dcfKeys;
+            string[] dcfAesKeys = sortRandomRequest.dcfAesKeys;
             int valuesIndex = 0;
 
             for (int i = 0; i < numOfElement; i++)
@@ -70,33 +104,13 @@ namespace MPCServer
                 }
             }
 
-            //DEBUG
-            Console.WriteLine("Sum Results");
-            for (int i = 0; i < sharesIndexes.Length; i++)
-            {
-                Console.WriteLine("\t" + sharesIndexes[i] + "\t");
-            }
-
-            // third level - compare eatch value result to all possible indexes and placing in the returned list
-            uint[] sortList = new uint[numOfElement];
-            string[] dpfKeys = sortRandomRequest.dpfKeys;
-            string[] dpfAesKeys = sortRandomRequest.dpfAesKeys;
-            uint[] sumIndexesMasks = sumIndexesWithMasks(sharesIndexes);
-
-            for (int i = 0; i < numOfElement; i++)
-            {
-                for (int j = 0; j < numOfElement; j++)
-                {
-                    sortList[j] += dpfAdapter.Eval(instance, dpfKeys[i], dpfAesKeys[i], sumIndexesMasks[i] - (uint)j, sumValuesMasks[i]);
-                }
-            }
-            return sortList;
+            return sharesIndexes;
         }
 
-        private uint[] SumEachPairValues(uint[] sumValuesMasks, int numOfElement)
+        private uint[] DiffEachPairValues(uint[] sumValuesMasks, int numOfElement)
         {
-            int nChoose2 = (numOfElement - 1) * numOfElement / 2;
-            uint[] diffValues = new uint[nChoose2];
+            int numOfElementChoose2 = (numOfElement - 1) * numOfElement / 2;
+            uint[] diffValues = new uint[numOfElementChoose2];
 
             int currIndex = 0;
 
@@ -111,16 +125,13 @@ namespace MPCServer
             return diffValues;
         }
 
-        private uint[] SumEachValueWithMask(uint[] sumValuesMasks)
+        private uint[] SumEachSharesWithMask(uint[] sumSharesMasks, uint[] sharesValue, uint[] masks)
         {
-            //uint[] sumValuesMasks = new uint[numOfElement];
-            uint[] masks = sortRandomRequest.dcfMasks;
-
-            for (int i = 0; i < sumValuesMasks.Length; i++)
+            for (int i = 0; i < sumSharesMasks.Length; i++)
             {
-                sumValuesMasks[i] += data[i] + masks[i];
+                sumSharesMasks[i] += sharesValue[i] + masks[i];
             }
-            return sumValuesMasks;
+            return sumSharesMasks;
         }
 
         private uint[] SumServersShares(int numOfElement)
@@ -130,14 +141,14 @@ namespace MPCServer
 
             if (instance == "A")
             {
-                uint[] sumValuesMasksA = SumEachValueWithMask(sumValuesMasks);
+                uint[] sumValuesMasksA = SumEachSharesWithMask(sumValuesMasks, data, sortRandomRequest.dcfMasks);
                 comm.SendServerData(sumValuesMasksA);
                 totalsumValuesMasks = comm.AReciveServerData();
             }
             else
             {
                 uint[] sumValuesMasksB = comm.BReciveServerData();
-                totalsumValuesMasks = SumEachValueWithMask(sumValuesMasksB);
+                totalsumValuesMasks = SumEachSharesWithMask(sumValuesMasks, data, sortRandomRequest.dcfMasks);
                 comm.SendServerData(totalsumValuesMasks);
             }
 
@@ -150,29 +161,18 @@ namespace MPCServer
 
             if (instance == "A")
             {
-                uint[] sumIndexesWithMasks = sumIndexesMasks(sharesIndexes);
+                uint[] sumIndexesWithMasks = SumEachSharesWithMask(sharesIndexes, sharesIndexes, sortRandomRequest.dpfMasks);
                 comm.SendServerData(sumIndexesWithMasks);
                 totalSumIndexesWithMasks = comm.AReciveServerData();
             }
             else
             {
-                totalSumIndexesWithMasks = comm.BReciveServerData();
-                totalSumIndexesWithMasks = sumIndexesMasks(totalSumIndexesWithMasks);
+                uint[] sumIndexesWithMasksA = comm.BReciveServerData();
+                totalSumIndexesWithMasks = SumEachSharesWithMask(sumIndexesWithMasksA, sharesIndexes, sortRandomRequest.dpfMasks);
                 comm.SendServerData(totalSumIndexesWithMasks);
             }
 
             return totalSumIndexesWithMasks;
-        }
-
-        private uint[] sumIndexesMasks(uint[] sharesIndexes)
-        {
-            uint[] sharesWithMasks = new uint[sharesIndexes.Length];
-
-            for (int i = 0; i < sharesIndexes.Length; i++)
-            {
-                sharesWithMasks[i] += sharesWithMasks[i] + sortRandomRequest.dpfMasks[i];
-            }
-            return sharesWithMasks;
         }
 
     }
