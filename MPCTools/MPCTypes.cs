c8328f262e41a668d0542e9a345222351307c5d6
MIDDLEEAST\edentanami <edentanami@microsoft.com>
2022-06-15 16:54:15 +0300

    increas n

--- MPCTools/MPCTypes.cs
+++ MPCTools/MPCTypes.cs
@@ -21,7 +21,7 @@ namespace MPCTools
     public class StateObject
     {
         public Socket workSocket = null;
-        public const int BufferSize = 2048;
+        public const int BufferSize = 30000;
         public byte[] buffer = new byte[BufferSize];
         public StringBuilder sb = new StringBuilder();
     }
